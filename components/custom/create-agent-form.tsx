112a0139758c43b54e71a8f64200e3a022dac11d
kurodenjiro <kurodenjiro1@gmail.com>
2024-11-07 17:14:01 +0700

    add create agent form

--- components/custom/create-agent-form.tsx
+++ components/custom/create-agent-form.tsx
@@ -1,7 +1,16 @@
+import { Cat, Dog, Fish, Rabbit, Turtle } from "lucide-react";
 import Form from 'next/form';
+import { useState } from "react";
 
+import { DEFAULT_MODEL_NAME, models } from '@/ai/models';
+
+import { MultiSelect } from '../custom/multi-select';
+import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 import { Label } from '../ui/label';
+import { Select, SelectItem, SelectContent, SelectGroup, SelectLabel, SelectTrigger, SelectValue } from '../ui/select';
+import { Textarea } from '../ui/textarea';
+
 
 export function CreateAgentForm({
   action,
@@ -10,8 +19,38 @@ export function CreateAgentForm({
   action: any;
   children: React.ReactNode;
 }) {
+  const [selectedFrameworks, setSelectedFrameworks] = useState<string[]>(["react", "angular"]);
+
+  const frameworksList = [
+    { value: "react", label: "React", icon: Turtle },
+    { value: "angular", label: "Angular", icon: Cat },
+    { value: "vue", label: "Vue", icon: Dog },
+    { value: "svelte", label: "Svelte", icon: Rabbit },
+    { value: "ember", label: "Ember", icon: Fish },
+  ];
   return (
     <Form action={action} className="flex flex-col gap-4 px-4 sm:px-16">
+
+      <div className="flex flex-col gap-2">
+        <Label
+          htmlFor="text"
+          className="text-zinc-600 font-normal dark:text-zinc-400"
+        >
+          Avatar
+        </Label>
+
+        <Input
+          id="avatar"
+          name="avatar"
+          className="bg-muted text-md md:text-sm"
+          type="text"
+          placeholder="Avatar"
+          autoComplete="text"
+          required
+          autoFocus
+          defaultValue={''}
+        />
+      </div>
       <div className="flex flex-col gap-2">
         <Label
           htmlFor="text"
@@ -38,18 +77,86 @@ export function CreateAgentForm({
           htmlFor="password"
           className="text-zinc-600 font-normal dark:text-zinc-400"
         >
-          Password
+          Description
         </Label>
 
-        <Input
-          id="password"
-          name="password"
+        <Textarea
+          id="Description"
+          name="Description"
           className="bg-muted text-md md:text-sm"
-          type="password"
           required
         />
       </div>
 
+      <div className="flex flex-col gap-2">
+        <Label
+          htmlFor="text"
+          className="text-zinc-600 font-normal dark:text-zinc-400"
+        >
+          Intro
+        </Label>
+
+        <Input
+          id="intro"
+          name="intro"
+          className="bg-muted text-md md:text-sm"
+          type="text"
+          required
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Label
+          htmlFor="text"
+          className="text-zinc-600 font-normal dark:text-zinc-400"
+        >
+          Model
+        </Label>
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select Model" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {models.map((model, index) => (
+                <SelectItem value={model.apiIdentifier} key={index}>{model.label}</SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      <div className="flex flex-col gap-2">
+        <Label
+          htmlFor="text"
+          className="text-zinc-600 font-normal dark:text-zinc-400"
+        >
+          Prompt
+        </Label>
+
+        <Textarea
+          id="prompt"
+          name="prompt"
+          className="bg-muted text-md md:text-sm"
+          required
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Label
+          htmlFor="text"
+          className="text-zinc-600 font-normal dark:text-zinc-400"
+        >
+          Tool
+        </Label>
+        <MultiSelect
+          name='tool'
+          options={frameworksList}
+          onValueChange={setSelectedFrameworks}
+          defaultValue={selectedFrameworks}
+          placeholder="Select frameworks"
+          variant="inverted"
+          animation={0}
+          maxCount={3}
+        />
+      </div>
       {children}
     </Form>
   );
