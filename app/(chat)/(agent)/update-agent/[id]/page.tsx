06adca63ff2414c709a745b39f21bea355c604ad
Kuro <mac@MacBook-Pro-cua-mac.local>
2025-01-09 18:24:37 +0700

    update

--- app/(chat)/(agent)/update-agent/[id]/page.tsx
+++ app/(chat)/(agent)/update-agent/[id]/page.tsx
@@ -3,20 +3,19 @@ import { getToolByUserId, getAgentById } from '@/db/queries';
 import UpdateAgent from './update-agent-form';
 
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-
-export default async function Page({ params }: PageProps) {
+export default async function Page({ params }: any) {
   const session = await auth();
 
   if (!session || !session.user) {
     return new Response('Unauthorized!', { status: 401 });
   }
+
   const { id } = await params;
+
+  if (!id) {
+    return new Response('Agent ID is required!', { status: 400 });
+  }
+
   const tools = await getToolByUserId({ userId: session.user.id! });
   const agent = await getAgentById(id);
 
